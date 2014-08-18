7c6db007304b402adf00f5889ceb2a76bd35fc35
Chanchal Santra <chanchal.santra@gmail.com>
2014-08-18 17:55:05 +0530

    google_analytics
    google give that code

--- analyticstracking.php
+++ analyticstracking.php
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-50929293-1', 'fungroo.com');
+  ga('create', 'xxxxx', 'website_name');
   ga('require', 'displayfeatures');
   ga('send', 'pageview');
 
